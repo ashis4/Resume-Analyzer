7ffc24c5322a4d5e568286d9bbb4f8dfb78b76d4
Ashish Ramchandra Gaikar <ashishgaikar2006@gmail.com>
2026-04-16 18:54:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
   res.send('Resume Analyzer API is running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
